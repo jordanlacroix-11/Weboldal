658f139b70936038a800517b33bf56b3f98ca596
jordanlacroix-11 <68813096+jordanlacroix-11@users.noreply.github.com>
2020-12-19 21:18:35 +0100

    1

--- js/Investing.js
+++ js/Investing.js
@@ -1,3 +1,19 @@
+function fv1() {
+    var nav = document.getElementById('nav');
+    window.onscroll = function(){
+        if (window.pageYOffset >100) {
+            nav.style.background = "rgba(0, 0, 0, 0.95";           
+            nav.style.boxShadow = "0px 2px 2px rgba(0, 0, 0, 0.65";
+            nav.style.transition = "0.3s";
+        }
+        else{
+            nav.style.background = "rgba(0, 0, 0, 0)";
+            nav.style.boxShadow = "none";
+            nav.style.transition = "0.3s";
+        }
+    }
+}
+
 function rendeles() {
     alert("Successfull order! Email sent with information.");
 }
